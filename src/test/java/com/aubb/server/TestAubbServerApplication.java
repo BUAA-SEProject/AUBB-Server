695a428e5ec56981c6a06ed667819dbdb0a5d834
MooreFoss <thelmh@outlook.com>
2026-04-14 15:27:36 +0800

    init

---
+++ src/test/java/com/aubb/server/TestAubbServerApplication.java
@@ -0,0 +1,11 @@
+package com.aubb.server;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestAubbServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(AubbServerApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
